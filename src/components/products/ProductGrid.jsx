918311cbf6abbbe5d2c5ffeabb3900ef4cf9b81d
David Andersson <lejonmanen@gmail.com>
2024-05-21 09:21:35 +0200

    Initial files

---
+++ src/components/products/ProductGrid.jsx
@@ -0,0 +1,15 @@
+
+
+const ProductGrid = () => {
+	const products = []  // TODO: hämta från zustand
+
+	return (
+		<div className="product-grid">
+			{products.map(product => (
+				'item'
+			))}
+		</div>
+	)
+}
+
+export default ProductGrid
